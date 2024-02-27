27f926b9252a9caa499adc149df95b6715a57d80
Flavio Silva <fal_silva@hotmail.com>
2024-02-26 23:32:02 -0300

    bd fake para teste

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake =[
+  {
+    "id":"1",
+    "title": "Novo Homem de Ferro Anunciado",
+    "description": "Marvel lançará novo filme do Iron Man",
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=webp"
+  },
+
+  {
+    "id":"2",
+    "title": "Nova série anunciada na Disney",
+    "description": "Novos episodios com os verdadeiros herois",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+
+]
